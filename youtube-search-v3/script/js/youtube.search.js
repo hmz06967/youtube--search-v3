4f91bfbbfcfda7baa1a69699f1b3731b4365785e
hmz06967 <hmz_06_1996@hotmail.com>
2015-10-06 01:18:55 +0300

    Update youtube.search.js

--- youtube-search-v3/script/js/youtube.search.js
+++ youtube-search-v3/script/js/youtube.search.js
@@ -1,3 +1,5 @@
+
+	//google api keyinizi aşağıya girdikten sonra script çalışır 
 	$(function(){  
 		$.ARamaYap = {
 			GetStringDuration: function (string){
@@ -14,8 +16,9 @@
 			var NextPage='&pageToken='+pagetoken,
 				aranan=query,
 				Max = 16,
-				JSONurl= 'https://www.googleapis.com/youtube/v3/search?part=snippet&q='+aranan+'&key=AIzaSyDBOCt3vmcrIWC9-LUGX3JtYkrNBwVqUgg&maxResults='+Max+'&type=video'+NextPage,
-				sonuc = $("#sonuc");
+				JSONurl= 'https://www.googleapis.com/youtube/v3/search?part=snippet&q='+aranan+'&key='+key+'&maxResults='+Max+'&type=video'+NextPage,
+				sonuc = $("#sonuc"),
+				key  = 'sizin google api key';
 			sonuc.empty();	
 				
 			$.getJSON(JSONurl ,function(data){
@@ -24,7 +27,7 @@
 
 			$.each(data.items,function(i,item){var kod=item.id.videoId,isim=item.snippet.title;
 				$(".Results").empty();$("._bpls[page-token=next]").show();
-				$.getJSON('https://www.googleapis.com/youtube/v3/videos?id='+kod+'&part=contentDetails&key=AIzaSyDXX-gRT3JDAwc25w3e_Q25Zwb6KkWrcoo',function(data){
+				$.getJSON('https://www.googleapis.com/youtube/v3/videos?id='+kod+'&part=contentDetails&key'+key,function(data){
 					
 					$.each(data.items,function(i,item){
 						
